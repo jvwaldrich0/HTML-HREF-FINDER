5909afb16c437c74b6d41eca6cb21bea72fc9b50
Joao Vitor Waldrich <jv.waldrich0@protonmail.com>
2024-03-10 20:42:50 -0300

    Build function - Desec Lab

--- parsing.sh
+++ parsing.sh
@@ -5,10 +5,14 @@ DEFAULT_INDEX_FILE="index.html"
 TEMP_FOLDER_NAME="tempScript"
 TARGET_URL=$1
 
+function filter_href_html {
+  grep "<body" -A 99999 | grep "href" | cut -d "=" -f 2 | cut -d ">" -f 1 | grep "\." | sed 's/"//g' | cut -d "/" -f 3
+}
+
 mkdir $TEMP_FOLDER_NAME && cd $TEMP_FOLDER_NAME
 wget $TARGET_URL
 
-data=$(cat $DEFAULT_INDEX_FILE | grep "<body" -A 99999 | grep "href" | cut -d "=" -f 2 | cut -d ">" -f 1 | grep "\." | sed 's/"//g' | cut -d "/" -f 3)
+data=$(cat $DEFAULT_INDEX_FILE | filter_href_html | sort | uniq)
 data_array=()
 
 while IFS=$'\n' read -r line; do
